f859cfef38dad146c35b4f96f04d7d9bff460097
xKamaiL <xkamail@icloud.com>
2021-08-13 16:04:34 +0700

    homework 1

--- main.c
+++ main.c
@@ -3,60 +3,65 @@
 
 int main(void)
 {
-	for (int i = 7; i >= 1; i--)
+
+	int grade[] = {0, 2, 3, 1, 2, 3, 4, 2, 1, 3, 2, 1, 2, 0, 3, 4, 4};
+	int count[5] = {0}; // grades counter
+	int avg;
+
+	int size = sizeof(grade) / sizeof(int); // size of array
+
+	int i = 0, j = 0;
+	int sum = 0, mode = 0, modeValue = 0;
+	while (i < size)
 	{
-		if (i > 2)
+		count[grade[i]]++;
+		if (mode < count[grade[i]])
 		{
-			printf("%d little ", i - 2);
+			mode = count[grade[i]];
+			modeValue = grade[i];
 		}
-		if (i == 2)
-		{
-			printf("Sad mother ");
-		}
+		sum += grade[i];
 
-		if (i == 1)
-		{
-			printf("Five little ");
-		}
-
-		printf("ducks went out");
-		if (i == 1)
-		{
-			printf(" to play\n");
-		}
-		else
-		{
-			printf(" one day\n");
-		}
-		printf("Over the hill and far away\n");
-		printf("Mother duck said, \"Quack, quack, quack, quack\"\n");
-		if (i <= 2)
-		{
-			printf("And all ");
-		}
-		else
-		{
-			printf("But ");
-		}
-		if (i >= 4)
-		{
-			printf("only %d", i - 3);
-		}
-		if (i == 3)
-		{
-			printf("none of ");
-		}
-
-		if (i <= 3)
-		{
-			printf("of the five");
-		}
-		printf(" little ducks came back\n");
-		for (int j = 1; j <= i - 3; j++)
-		{
-			printf("%d ", j);
-		}
-		printf("\n");
+		i++;
 	}
+
+	int median = 0, temp;
+	// sort
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - i; j++)
+		{
+			if (grade[j] > grade[j + 1])
+			{
+				temp = grade[j + 1];
+				grade[j + 1] = grade[j];
+				grade[j] = temp;
+			}
+		}
+	}
+	int medianPos = (size + 1) / 2;
+	for (i = 0; i < size; i++)
+	{
+		printf("%d ", grade[i]);
+	}
+	printf("\n");
+	printf("_________________________\n");
+	printf("|\tgrade\t|\tamount\t|\n");
+	printf("-------------------------\n");
+
+	i = 0;
+	while (i < 5)
+	{
+		printf("|\t");
+		printf("%3d\t\t|", i);
+		printf("\t%4d\t|\n", count[i]);
+		i++;
+	}
+
+	printf("-------------------------\n");
+	printf("mean average grade = %.2f\n", sum * 1.0 / size);
+	printf("mode grade = %d\n", modeValue);
+	printf("median grade = %d\n", grade[medianPos]);
+
 	return 0;
 }
