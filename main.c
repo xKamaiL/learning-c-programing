f12a67f8fa025098cf8a053e2c7f295a00a0721f
xKamaiL <xkamail@icloud.com>
2021-08-17 23:51:22 +0700

    feat: question two

--- main.c
+++ main.c
@@ -3,65 +3,57 @@
 
 int main(void)
 {
-
-	int grade[] = {0, 2, 3, 1, 2, 3, 4, 2, 1, 3, 2, 1, 2, 0, 3, 4, 4};
-	int count[5] = {0}; // grades counter
-	int avg;
-
-	int size = sizeof(grade) / sizeof(int); // size of array
-
-	int i = 0, j = 0;
-	int sum = 0, mode = 0, modeValue = 0;
-	while (i < size)
+	int max = 0, isInvalid = 0;
+	printf("How many values (max=10): ");
+	// get max value
+	scanf("%d", &max);
+	// initial array by max size
+	int data[max];
+	printf("Enter data: ");
+	// loop according to max size
+	for (int i = 0; i < max; i++)
 	{
-		count[grade[i]]++;
-		if (mode < count[grade[i]])
+		// scan into data[index]
+		scanf("%d", &data[i]);
+		if (0 < data[i] && data[i] > 100)
 		{
-			mode = count[grade[i]];
-			modeValue = grade[i];
+			isInvalid = 1;
+			printf("Value is invalid.\n");
+			break;
 		}
-		sum += grade[i];
-
-		i++;
 	}
-
-	int median = 0, temp;
-	// sort
-	for (i = 0; i < size; i++)
+	if (isInvalid == 1)
 	{
-		for (j = 0; j < size - i; j++)
+		return 0;
+	}
+	printf("Sorted data: ");
+	// start sorting
+	// order from last position to first
+	for (int i = 0; i < max - 1; i++)
+	{
+		// the last i elements are already in right position
+		for (int j = 0; j < max - i - 1; j++)
 		{
-			if (grade[j] > grade[j + 1])
+			// check if next value is higher than or not
+			if (data[j] > data[j + 1])
 			{
-				temp = grade[j + 1];
-				grade[j + 1] = grade[j];
-				grade[j] = temp;
+				// if j + 1 is higher
+				// swap the position
+				// j + 1 to j
+				// j to j + 1
+				int temp = data[j + 1];
+				data[j + 1] = data[j];
+				data[j] = temp;
 			}
 		}
 	}
-	int medianPos = (size + 1) / 2;
-	for (i = 0; i < size; i++)
+	// print the result of sorted array
+	for (int i = 0; i < max; i++)
 	{
-		printf("%d ", grade[i]);
+		printf("%d ", data[i]);
 	}
+	// new line
 	printf("\n");
-	printf("_________________________\n");
-	printf("|\tgrade\t|\tamount\t|\n");
-	printf("-------------------------\n");
-
-	i = 0;
-	while (i < 5)
-	{
-		printf("|\t");
-		printf("%3d\t\t|", i);
-		printf("\t%4d\t|\n", count[i]);
-		i++;
-	}
-
-	printf("-------------------------\n");
-	printf("mean average grade = %.2f\n", sum * 1.0 / size);
-	printf("mode grade = %d\n", modeValue);
-	printf("median grade = %d\n", grade[medianPos]);
 
 	return 0;
 }
