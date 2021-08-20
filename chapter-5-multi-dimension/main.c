8708b4729f2777569e485a713d1a1a5daa962fed
xKamaiL <xkamail@icloud.com>
2021-08-20 17:01:53 +0700

    add chapter 5

--- chapter-5-multi-dimension/main.c
+++ chapter-5-multi-dimension/main.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
-#define ROW 3;
-#define COL 3;
 
-int main()
-{
-	int a[ROW][COL] = {
-		{0, 1, 2},
-		{3, 4, 5},
-		{6, 7, 8}};
+#define ROW 9
+#define COL 9
 
-	for (int i = 0; i < ROW; i++)
-	{
-		for (int j = 0; j < COL; j++)
-		{
-			printf("%d ", a[j][i]);
-		}
-	}
-	return 0;
+int main(void) {
+    int draw[ROW][COL] = {{0, 0, 1, 1, 0, 1, 1, 0, 0},
+                          {0, 1, 0, 0, 1, 0, 0, 1, 0},
+                          {1, 0, 0, 0, 0, 0, 0, 0, 1},
+                          {1, 0, 0, 0, 0, 0, 0, 0, 1},
+                          {0, 1, 0, 0, 0, 0, 0, 1, 0},
+                          {0, 0, 1, 0, 0, 0, 1, 0, 0},
+                          {0, 0, 0, 1, 0, 1, 0, 0, 0},
+                          {0, 0, 0, 0, 1, 0, 0, 0, 0}};
+    // transpose
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            int temp = draw[i][j];
+            draw[i][j] = draw[j][i];
+            draw[j][i] = temp;
+        }
+    }
+    //
+    printf(" #| ");
+    for (int i = 0; i < COL; i++) {
+        printf("%d ", i);
+    }
+    printf("\n--+");
+    for (int i = 0; i < COL; i++) {
+        printf("--");
+    }
+    printf("\n");
+    for (int i = 0; i < ROW; i++) {
+        printf(" %d|", i);
+        for (int j = 0; j < COL; j++) {
+            if (draw[i][j] == 1) {
+                printf(" \u2593");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
 }
---
+++ cmake-build-debug/CMakeFiles/hello_world.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_world.dir/chapter-4-array/main.c.o"
+  "CMakeFiles/hello_world.dir/chapter-5-multi-dimension/main.c.o"
+  "hello_world"
+  "hello_world.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hello_world.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
