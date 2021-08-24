f55d0086f77b554058949d910b4fcfe24adae1f8
xKamaiL <xkamail@icloud.com>
2021-08-24 20:15:10 +0700

    hoemwork 1

--- chapter-5-multi-dimension/homework-1.c
+++ chapter-5-multi-dimension/homework-1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main() {
     int data[4][4] = {
             {21, 20, 45, 35},
@@ -10,22 +9,33 @@ int main() {
     };
 
     printf("Enter a value to find: ");
+    // declare input value as a int
     int find;
+    // get input from user
     scanf("%d", &find);
+    // set index of row , col to -1
+    // which mean not found
     int indexRow = -1, indexCol = -1;
+    // loop row
     for (int i = 0; i < 4; i++) {
+        // lop col
         for (int j = 0; j < 4; j++) {
+            // if  value equal to find
             if (data[i][j] == find) {
+                // set index row to current i
                 indexRow = i;
+                // set index col to current j
                 indexCol = j;
                 break;
             }
         }
     }
+    // index be -1 means not found
     if (indexRow == -1 || indexCol == -1) {
         printf("%d is not found.\n", find);
         return 0;
     }
+    // found show the index (col,row)
     printf("%d is found at row %d, column %d\n", find, indexRow, indexCol);
     return 0;
 }
--- chapter-5-multi-dimension/homework-2.c
+++ chapter-5-multi-dimension/homework-2.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+// calculate matrix det
+
+int main() {
+    int A[3][3];
+    for (int i = 0; i < 3; i++) {
+        printf("Enter element on row [%d]:", i);
+        scanf("%d %d %d", &A[i][0], &A[i][1], &A[i][2]);
+    }
+    return 0;
+}
