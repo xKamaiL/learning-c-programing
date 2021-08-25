b714705e142393292a1c1e0336f6576022c3f7a5
xKamaiL <xkamail@icloud.com>
2021-08-25 12:40:56 +0700

    update homework 5

--- chapter-5-multi-dimension/homework-2.c
+++ chapter-5-multi-dimension/homework-2.c
@@ -2,10 +2,21 @@
 // calculate matrix det
 
 int main() {
+    // declare array 3x3
     int A[3][3];
+    // loop 3 row
     for (int i = 0; i < 3; i++) {
         printf("Enter element on row [%d]:", i);
+        // get input to col 0,1,2
         scanf("%d %d %d", &A[i][0], &A[i][1], &A[i][2]);
     }
+    // calculate from formula
+    int detA = (A[0][0] * A[1][1] * A[2][2]) +
+               (A[0][1] * A[1][2] * A[2][0]) +
+               (A[0][2] * A[1][0] * A[2][1])
+               - (A[2][0] * A[1][1] * A[0][2])
+               - (A[2][1] * A[1][2] * A[0][0])
+               - (A[2][2] * A[1][0] * A[0][1]);
+    printf("\nDetA = %d\n", detA);
     return 0;
 }
--- chapter-5-multi-dimension/homework-3.c
+++ chapter-5-multi-dimension/homework-3.c
@@ -2,14 +2,18 @@
 
 
 int main() {
+    // declare 3x3
     int dice[3][3] = {
             {0, 0, 0},
             {0, 0, 0},
             {0, 0, 0},
     };
+    // declare page
     int page = 0;
     printf("dice face: ");
+    // get page from user
     scanf("%d", &page);
+    // relation of dice / page
     if (page % 2 != 0) {
         dice[1][1] = 1;
     }
@@ -19,25 +23,22 @@ int main() {
     }
     if (page >= 4) {
         dice[0][2] = 1;
+        dice[2][0] = 1;
     }
     if (page == 6) {
-        dice[0][0] = 1;
         dice[1][0] = 1;
-        dice[2][0] = 1;
-
-        dice[0][2] = 1;
         dice[1][2] = 1;
-        dice[2][2] = 1;
     }
-
     printf("= = =\n");
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
+
             if (dice[i][j] == 1) {
                 printf(" *");
             } else {
                 printf(" ");
             }
+
         }
         printf("\n");
     }
